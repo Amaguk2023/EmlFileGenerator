9d1831900fe667c36ea625e744df0e85d67d6d98
Amaguk2023 <ernestomanuel2023@gmail.com>
2021-06-25 11:25:05 +0200

    changed program name

---
+++ eml_file_generator.py
@@ -0,0 +1,60 @@
+import string
+from datetime import datetime
+import sys
+
+
+#EMAIL METADATA AND CONTENT PROMPT
+def email_metadata_content_prompt():
+	try:
+
+		from_ = input('From: ').lower()
+		sent_ = input('Sent: (T = Today / N = New Date) ').title()
+		if sent_ == 'T':
+			sent_ = datetime.now()
+
+		else:
+			sent_ = input('Sent: ').title()
+		
+		to_ = input('To: ').lower()
+		subject_ = input('Subject: ').title()
+		content_list = []
+		print('\nContent: (Use \'AGKdone\' for saving)\n')
+		while True:
+			line = input()
+			if 'AGKdone' in line:
+				break
+			content_list.append(line)
+			content = ' '.join(content_list)
+		eml_metadata_content_wrap(from_, sent_, to_, subject_, content)
+					
+	except KeyboardInterrupt:
+		print('\nGoodbye\n')
+		sys.exit()
+
+	
+#WRAP THE METADATA AND CONTENT IN ORDER
+def eml_metadata_content_wrap(from_, sent_, to_, subject_, content):
+	eml_From = ('From: {from_}'.format(from_=from_))
+	eml_Sent= ('Sent: {sent_}'.format(sent_=sent_))
+	eml_To= ('To: {to_} '.format(to_=to_))
+	eml_Subject = ('Subject: {subject_}'.format(subject_=subject_))
+	eml = eml_From + '\n' + eml_Sent + '\n' + eml_To + '\n' + eml_Subject + '\n' + '\n' + content
+	eml_file_creation(eml)
+
+
+#OPEN .EML FILE, SAVE, EXPORT.
+def eml_file_creation(eml):
+	name = input('\nWrite down file name (Don\'t include \'.eml\' ) >> ')
+	with open(name + '.eml', 'w') as email:
+		email.write(''.join(eml))
+		print('\neml file has been exported!\n')
+
+#FIRST MESSAGE, FIRST CALL
+print('\nWelcome to AGK eml file generator\n')
+email_metadata_content_prompt()
+
+
+
+
+
+
